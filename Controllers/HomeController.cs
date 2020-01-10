928a04c07d5f8e097773bab25378505781417a9c
BhattAneri <200430090@student.georgianc.on.ca>
2020-01-09 20:13:51 -0500

    My First Commit!

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            ViewBag.Sentence = "This comes from the controller.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            ViewData["FromController"] = "This view is from the controller";
+            return View();
+        }
+        public ActionResult Services()
+        {
+            ViewData["FromController"] = "We offer lots of great Services.";
+            return View();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+NAME: Aneri Bhatt
+Student Number : 200430090
+Student Email : 200430090@student.georgianc.on.ca
